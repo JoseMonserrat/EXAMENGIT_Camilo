3e196dc9acb243b2ea1fbff80db07cc6b7e4a978
Jose <jose.monserratcuello@gmail.com>
2019-10-24 20:42:09 +0200

    mejora2

--- P216.java
+++ P216.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class P216 {
 
  public static void main( String[] args ) {
-    
+//Declarmos la clase Scanner y las variables    
 	Scanner sc = new Scanner (System.in); 
     
      	int ncasos, gota;
      	int c; 
      
      	ncasos = sc.nextInt();
-     
+//iniciamos el bucle for     
      	for (c = 0; c < ncasos; c++) {
-         gota = sc.nextInt();
-         
+//asignamos las nuevas variables         
+	gota = sc.nextInt();         
        	int  d = gota / 3600;
        	int b = (gota / 60) - (d * 60) ;
        	int  a = gota - (b * 60) - (d * 3600);            
-        
+//mostramos el resultado final        
         System.out.printf("%02d:%02d:%02d\n", d, b, a);
      }
      
